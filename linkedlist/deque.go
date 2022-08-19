4341f7f2341b1272dd13b14dbdb1ce8d2932a09b
Anthony Dito <anthonymdito@gmail.com>
2022-08-19 00:52:26 -0700

    add some comments to deque

--- linkedlist/deque.go
+++ linkedlist/deque.go
@@ -5,12 +5,19 @@
 package linkedlist
 
 type Deque[T any] interface {
+	// Len The number of items in the Deque
 	Len() int
+	// RemoveFirst Removes the first item, returning the value
 	RemoveFirst() T
+	// PeekFirst Returns the first item
 	PeekFirst() T
+	// AddFirst Adds an item to the start of the deque
 	AddFirst(T)
+	// RemoveLast Removes the last item, returning the value
 	RemoveLast() T
+	// AddLast Adds an item to the end of the deque
 	AddLast(T)
+	// PeekLast Removes the last item from the deque
 	PeekLast() T
 }
 
